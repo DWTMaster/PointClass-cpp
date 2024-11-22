d429e30bb550aff5142cfa59590635041d063e5c
anton <dregswanten@gmail.com>
2024-11-22 22:56:30 +0700

    Fixed bags with operators (*, /), added new functions, finding distance to simple geometric objects, added checks if _GLIBCXX_DEBUG is defined

--- Point.cpp
+++ Point.cpp
@@ -24,21 +24,49 @@ class TPoint {
     void rotate_counterclockwise() { swap(x, y); x *= -1; }
 
     // the square of the length (can be useful with integer types)
-    [[nodiscard]] auto len2() const -> decltype(int64_t{}) {
-        if constexpr (std::is_integral_v<Type>)
+    [[nodiscard]] auto len2() const {
+        if constexpr (std::is_integral_v<Type>) {
             return static_cast<long long>(x) * x + static_cast<long long>(y) * y;
-        return static_cast<long double>(x) * x + static_cast<long double>(y) * y;
+        } else {
+            return static_cast<long double>(x) * x + static_cast<long double>(y) * y;
+        }
     }
     [[nodiscard]] long double len() const { return sqrt(len2()); }
+
+    [[nodiscard]] long double dist_to_point(const TPoint &other) const { return (*this - other).len(); }
+    [[nodiscard]] long double dist_to_line(const TPoint &p1, const TPoint &p2) const {
+#ifdef _GLIBCXX_DEBUG
+        if (p1 == p2) throw std::runtime_error("A line cannot be defined by a single point\n");
+#endif
+        return abs((p1 - *this) ^ (p2 - *this)) / (p1 - p2).len();
+    }
+    [[nodiscard]] long double dist_to_ray(const TPoint &start, const TPoint &other) const {
+#ifdef _GLIBCXX_DEBUG
+        if (start == other) throw std::runtime_error("A ray cannot be defined by a single point\n");
+#endif
+        if ((*this - start) * (other - start) >= 0)
+            return dist_to_line(start, other);
+        return dist_to_point(start);
+    }
+    [[nodiscard]] long double dist_to_section(const TPoint &p1, const TPoint &p2) const {
+        if ((*this - p1) * (p2 - p1) >= 0 && (*this - p2) * (p1 - p2) >= 0) {
+            if (p1 == p2) return dist_to_point(p1);
+            return dist_to_line(p1, p2);
+        }
+        return min(dist_to_point(p1), dist_to_point(p2));
+    }
 
     // check for being in the upper half-plane
     [[nodiscard]] bool is_upper() const { return y > eps || (abs(y) <= eps && x > eps); }
 
     // polar angle comparison
-    // -1 - this point degree is smaller
+    // -1 - this point angle is smaller
     //  0 - they are equal
-    //  1 - this point degree is bigger
+    //  1 - this point angle is bigger
     [[nodiscard]] int cmp_polar(const TPoint &other) const {
+#ifdef _GLIBCXX_DEBUG
+        if (!*this || !other) throw std::runtime_error("The angle is undefined for the point (0; 0)\n");
+#endif
         if (const bool a = is_upper(), b = other.is_upper(); a != b) { return a ? -1 : 1; }
         Type cross_prod = *this ^ other;
         return cross_prod > eps ? -1 : cross_prod < -eps ? 1 : 0;
@@ -58,8 +86,11 @@ class TPoint {
     template<typename T> TPoint &operator /=(const T &value) {
         x /= static_cast<Type>(value); y /= static_cast<Type>(value); return *this;
     }
-    TPoint operator -() { return TPoint(-x, -y, id); }
-    bool operator !() { return abs(x) <= eps && abs(y) <= eps; }
+    TPoint operator -() const { return TPoint(-x, -y, id); }
+    bool operator !() const { return abs(x) <= eps && abs(y) <= eps; }
+
+    template<typename VT> TPoint operator *(const VT &value) const { return TPoint(x * value, y * value); }
+    template<typename VT> TPoint operator /(const VT &value) const { return TPoint(x / value, y / value); }
 
     auto operator *(const TPoint &other) const {
         if constexpr (std::is_integral_v<Type>) {
@@ -83,9 +114,6 @@ class TPoint {
         }
     }
 
-    template<typename T, typename VT> friend TPoint<T> operator *(const TPoint<T> &p, const VT &value);
-    template<typename T, typename VT> friend TPoint<T> operator /(const TPoint<T> &p, const VT &value);
-
     // the comparison of points is all follows:
     // the lowest point is considered the smallest, of which the leftmost is considered
     template<typename T1, typename T2> friend bool operator ==(const TPoint<T1> &a, const TPoint<T2> &b);
@@ -95,19 +123,9 @@ class TPoint {
     template<typename T1, typename T2> friend bool operator <=(const TPoint<T1> &a, const TPoint<T2> &b);
     template<typename T1, typename T2> friend bool operator >=(const TPoint<T1> &a, const TPoint<T2> &b);
 
-    template<typename T> friend TPoint<T> operator +(const TPoint<T> &a, const TPoint<T> &b);
-    template<typename T> friend TPoint<T> operator -(const TPoint<T> &a, const TPoint<T> &b);
-
     template<typename ST, typename T> friend ST& operator >>(ST& stream, TPoint<T> &p);
     template<typename ST, typename T> friend ST& operator <<(ST& stream, const TPoint<T> &p);
 };
-
-template<typename T, typename VT> TPoint<T> operator *(const TPoint<T> &p, const VT &value) {
-    p.x *= value; p.y *= value; return p;
-}
-template<typename T, typename VT> TPoint<T> operator /(const TPoint<T> &p, const VT &value) {
-    p.x /= value; p.y /= value; return p;
-}
 
 template<typename T1, typename T2> bool operator ==(const TPoint<T1> &a, const TPoint<T2> &b) {
     return a.x == b.x && a.y == b.y;
